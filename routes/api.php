2e400930b8042e39f32b88ca4a4e61c81d2732a4
kaunghtetzaw <kaunghtetzaw.khz9@gmail.com>
2019-06-24 22:12:00 +0630

    add route api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::resource('products', 'Api\ProductController');
+Route::resource('townships', 'Api\TownshipController');
+Route::resource('users', 'Api\UserController');
