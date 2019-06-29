49c0354328e7475234deca692698eeabebcd338d
kaunghtetzaw <kaunghtetzaw.khz9@gmail.com>
2019-06-29 11:03:34 +0630

    test"

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -16,27 +16,36 @@ class OrderController extends Controller
 
             {
 
-              $products = $request->json()->all();
+              // $products = $request->json()->all();
 
-              if($products) {
+              // if($products) {
 
-                foreach($products as $id => $product) {
+              //   foreach($products as $id => $product) {
 
+              //     $order_detail = new Order_detail();
+              //     $order_detail->name = $product['name'];
+              //     $order_detail->quantity = $product['quantity'];
+              //     $order_detail->price = $product['price'];
+              //     $order_detail->user_id = $product['user_id'];
+              //     $order_detail->save();
+
+              //     $data = $order_detail->toArray();
+
+
+              //   }
+              //   return response()->json($data, 200);
+              // }
+              
+              
+                  $product = $request->products;
                   $order_detail = new Order_detail();
                   $order_detail->name = $product['name'];
-                  $order_detail->quantity = $product['quantity'];
-                  $order_detail->price = $product['price'];
-                  $order_detail->user_id = $product['user_id'];
+                  $order_detail->quantity = (int)$product['quantity'];
+                  $order_detail->price = (int)$product['price'];
+                  $order_detail->user_id = (int)$product['user_id'];                      
                   $order_detail->save();
 
                   $data = $order_detail->toArray();
-
-
-                }
-                return response()->json($data, 200);
-              }
-              
-              
 
                 return response()->json($products, 200);
             }
