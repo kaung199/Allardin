4ec2ab8bf7976cf70ede8638bc8fabc9f205eb78
kaung199 <kaunghtetzaw.khz9@gmail.com>
2019-07-22 14:51:54 +0630

    chang css

--- public/js/app.js
+++ public/js/app.js
@@ -67004,7 +67004,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "container pt-3 pb-3" }, [
+  return _c("div", { staticClass: "container paddingtop pb-3" }, [
     _c("div", { staticClass: "row justify-content-center" }, [
       _c(
         "div",
