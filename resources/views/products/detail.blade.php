4eaf53096a8858d0dafb2eff8658ad0150107b08
kaung199 <kaunghtetzaw.khz9@gmail.com>
2019-09-21 14:29:32 +0630

    add font

--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -9,7 +9,11 @@
         <div class="col-md-4 pb-3">
             <div class="card" style="width: 18rem;">
                 <a href="{{ route('productdetail', $product->id) }}">
-                    <img src="{{ asset('storage/' . $product->photos[0]->filename) }}" class="card-img-top" alt="...">
+                    @if($product->photos[0][filename])
+                        <img src="{{ asset('storage/' . $product->photos[0]->filename) }}" class="card-img-top" alt="...">
+                    @else
+                    <img src="{{ asset('/ui/images/350400.jpg') }}" class="card-img-top" alt=""/>
+                    @endif
                 </a>
                 <div class="card-body">
                     <h5 class="card-title">{{ $product->name }}</h5>
