7505345289bd57ebc2450d16817d6a4981dd290a
Kaung Htet Zaw <kaunghtetzaw@Kaungs-MacBook-Pro.local>
2020-05-08 20:52:09 +0630

    add g

---
+++ resources/views/products/getapp.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.firstlayout')
+
+@section('title')
+    Get App
+@endsection
+
+@section('contents')
+<div class="getapp">
+    <div style="padding-top:180px; color:white;">
+        <h3 class="text-center">Download For Your Device
+        </h3>
+    </div>
+    <div style="padding-top:10px; color:#948080;">
+        <h5 class="text-center">Get Aladdin Online Shop App on</h5>
+    </div>
+    <div class="text-center" style="padding-top:10px;">
+		<a href="https://play.google.com/store/apps/details?id=com.ppl.aladdin" id="android">
+			<img src="https://i.ibb.co/Hr8Q2Hh/google-play.png" alt="google-play" border="0" style="width:200px;heigt:100px;">
+		</a>
+    </div>
+</div>
+    
+@endsection
